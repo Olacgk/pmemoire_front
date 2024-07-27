e2e0c08524996215f5127ea6690916d02e9cccbc
Theotime Chognika <94149002+Olacgk@users.noreply.github.com>
2024-07-27 09:49:07 +0000

    dashboard header review

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -11,15 +11,6 @@ export default function HeaderDash() {
             {/* <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" /> */}
           </Link>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            {/* <Bars3Icon aria-hidden="true" className="h-6 w-6" /> */}
-          </button>
-        </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <Link to="#" className="text-sm font-semibold leading-6 text-gray-900">
             Se déconnecter <span aria-hidden="true">&rarr;</span>
